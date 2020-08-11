45864aba194140debfde8ebe40124ffafb644df6
Deep283 <deepmwn@gmail.com>
2020-08-11 09:27:26 +0530

    Find All Numbers Disappeared in an Array  Solution
    Given an array of integers where 1 ≤ a[i] ≤ n (n = size of array), some elements appear twice and others appear once.
    
    Find all the elements of [1, n] inclusive that do not appear in this array.
    
    Could you do it without extra space and in O(n) runtime? You may assume the returned list does not count as extra space.

---
+++ findDisappearedNumbers.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> findDisappearedNumbers(vector<int>& nums) {
+        
+        int j,n = nums.size();
+        for(int i=0;i<n;i++){
+            j = abs(nums[i])-1;
+            nums[j] = abs(nums[j]) * -1;  
+        }
+        vector<int> res;
+        for(int i=0;i<n;i++){
+            if(nums[i] > 0)
+                res.push_back(i+1);
+        }
+        return res;
+    }
+};
